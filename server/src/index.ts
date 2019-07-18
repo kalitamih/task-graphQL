8f1c603e78894bde33d055c24b217e326dec8bc8
Mikhail <mkalita1991@gmail.com>
2019-07-19 01:43:40 +0300

    fix: gitignore file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,33 @@
-import cors from '@koa/cors';
-import { ApolloServer, gql } from 'apollo-server-koa';
-import jwt from 'jsonwebtoken';
-import Koa from 'koa';
-import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors'
+import { ApolloServer, gql } from 'apollo-server-koa'
+import jwt from 'jsonwebtoken'
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
 
-import resolvers from './resolvers';
-import typeDefs from './schema';
+import resolvers from './resolvers'
+import typeDefs from './schema'
 
-import { KEY, PORT } from '../constants';
+import { KEY, PORT } from '../constants'
 
 const server = new ApolloServer({
   context: async ({ ctx }) => {
-    const token = ctx.headers.authorization;
-    let currentUser: string | object = '';
+    const token = ctx.headers.authorization
+    let currentUser: string | object = ''
     if (token) {
-      currentUser = await jwt.verify(token, KEY);
-      return { currentUser };
+      currentUser = await jwt.verify(token, KEY)
+      return { currentUser }
     }
-    return { currentUser };
+    return { currentUser }
   },
   resolvers,
   typeDefs,
-});
+})
 
-const app = new Koa();
+const app = new Koa()
 
-app.use(cors());
-app.use(bodyParser());
+app.use(cors())
+app.use(bodyParser())
 
-server.applyMiddleware({ app });
+server.applyMiddleware({ app })
 
-app.listen({ port: PORT });
+app.listen({ port: PORT })
