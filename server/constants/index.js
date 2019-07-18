c5ae260afbf9390b86b8d6bcb40927fa468dd6e8
Mikhail <mkalita1991@gmail.com>
2019-07-19 01:53:48 +0300

    fix: gitignore file

--- server/constants/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KEY = process.env.KEY || 'Jw2pK7JS';
-exports.PORT = process.env.PORT || 4000;
-exports.PASSWORD = '$2b$10$9uCRax68TUjDs3WJSIRV1O6lmIISkNMee5SP0OwGOGHPTwIYVN6LC';
-exports.TEST_USER = 'kalitamih';
-exports.LINK = 'http://localhost:4000/graphql';
