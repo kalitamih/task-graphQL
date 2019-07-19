488c2bcc67df3da5611bfcb4375712c41fdc1562
Vladislav Polyakov <39828645+polRk@users.noreply.github.com>
2019-07-19 10:10:45 +0300

    Review: WithSession
    Не стоит использовать props, У apollo очень хорошие дженерики, их стоит использовать

--- client/src/components/WithSession/index.tsx
+++ client/src/components/WithSession/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Query, QueryResult } from 'react-apollo';
 import { GET_USER_INFO } from '../../queries';
 
-const withSession = (Component: React.FC) => (props: any) => (
+const withSession = (Component: React.FC) => ( props: any ) => (
   <Query query={GET_USER_INFO}>
     {({ data, loading, refetch }: QueryResult) => {
       if (loading) {
